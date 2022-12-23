575c824c8370118dbadf5b59b59ba03b30f161c0
chrismenke45 <chrismenke45@gmail.com>
2022-12-22 16:18:56 -0800

    for each indiviudal post shown on feed

---
+++ src/components/PostOnFeed.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PostOnFeed: React.FC = () => {
+
+
+    return (
+        <div>
+            <h1>PostOnFeed</h1>
+        </div>
+    );
+}
+
+export default PostOnFeed;
