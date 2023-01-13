a550f30af8fe232960967b6479727ade480b09a1
chrismenke45 <chrismenke45@gmail.com>
2023-01-13 10:18:07 -0800

    doesnt add data if it is falsy

--- src/functions/fetch/buildFormData.ts
+++ src/functions/fetch/buildFormData.ts
@@ -2,7 +2,7 @@
 const buildFormData = (formInfoArray: string[][]): FormData => {
     let data = new FormData()
     formInfoArray.forEach(formInfo => {
-        data.append(formInfo[0], formInfo[1])
+        if (formInfo[1]) { data.append(formInfo[0], formInfo[1]) }
     })
     return data
 }
