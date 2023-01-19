907ff20179773eb68618aa650103aaf9bf4f617e
chrismenke45 <chrismenke45@gmail.com>
2023-01-19 09:35:24 -0800

    renamed class, made default method 'GET', still good OOP

---
+++ src/functions/fetch/FetchAPI.ts
@@ -0,0 +1,50 @@
+interface FetchObjectProp {
+    options: RequestInit
+}
+
+class FetchAPI {
+    options: RequestInit;
+    constructor() {
+        this.options = {
+            method: "GET",
+            mode: 'cors',
+            headers: {
+                'Accept': 'application/json',
+            }
+        }
+    }
+    buildFormData(formInfoArray: string[][]): void {
+        let data = new FormData()
+        formInfoArray.forEach(formInfo => {
+            if (formInfo[1]) { data.append(formInfo[0], formInfo[1]) }
+        })
+        this.options.body = data
+    }
+    fetchData(path: string, method?: string, authToken?: string): Promise<any> {
+        //this fetches data from the api at a certain path
+        let url: string = `${process.env.REACT_APP_API_URL}/${path}`;
+        this.options.method = method
+        if (authToken) {
+            this.options.headers = {
+                ...this.options.headers,
+                'Authorization': `Bearer ${authToken}`,
+            }
+        }
+        return fetch(url, this.options)
+            .then(response => response.json())
+            .then(responseObject => responseObject)
+            .catch(error => {
+                console.error('Error:', error)
+            })
+    }
+    loginAsGuest() {
+        this.buildFormData([["auth[username]", "guest"], ["auth[password]", process.env.REACT_APP_GUEST_PASSWORD || ""]])
+        this.fetchData("/auth/login", "POST")
+        .then(data => {
+            return data
+        })
+        .catch(err => console.error(err))
+    }
+}
+
+export default FetchAPI
