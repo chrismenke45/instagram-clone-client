0b4a36e062cabd0027235c99972414c452cce0d4
chrismenke45 <chrismenke45@gmail.com>
2023-02-08 20:36:11 -0800

    conversation allows user to create message

--- src/pages/ConversationPage.tsx
+++ src/pages/ConversationPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import BackBanner from '../components/outer/BackBanner';
 import FetchAPI from '../functions/fetch/FetchAPI';
 import getUserObject from '../functions/user/getUserObject';
@@ -11,6 +11,7 @@ const ConversationPage: React.FC = () => {
     const fetcher = new FetchAPI()
     const user = getUserObject()
     const [messages, setMessages] = useState<ConversationProp[]>([])
+    const [newMessage, setNewMessage] = useState<string>("")
     const [otherUser, setOtherUser] = useState<any>(null)
     const { user_id } = useParams()
 
@@ -22,17 +23,38 @@ const ConversationPage: React.FC = () => {
     }, [])
 
     useEffect(() => {
-        fetcher.fetchData(`users/${user_id}/messages/${user_id}`, "GET", user.jwt)
+        fetcher.fetchData(`users/${user.user_id}/messages/${user_id}`, "GET", user.jwt)
             .then(theMessages => {
                 setMessages(theMessages)
             })
     }, [])
 
+
+    const handleNewMessageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewMessage(e.target.value)
+    }
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (newMessage) {
+        fetcher.buildFormData([
+            ["message[text]", newMessage], 
+            ["message[receiver_id]", user_id || ""]
+        ])
+        fetcher.fetchData(`/users/${user.user_id}/messages`, "POST", user.jwt)
+            .then(data => {
+                console.log(data)
+            })
+        }
+    }
+
     return (
         <div id="page">
             <BackBanner header={otherUser?.username || "Conversation"} />
             <Conversation messages={messages} />
-
+            <form id="messageForm" onSubmit={handleSubmit}>
+                <input type="text" value={newMessage} onChange={handleNewMessageChange}></input>
+                <button type='submit'>Send</button>
+            </form>
         </div>
     );
 }
