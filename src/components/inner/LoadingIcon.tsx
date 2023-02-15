8b0acf001f325f9f2aa228466341664ed54a0bfb
chrismenke45 <chrismenke45@gmail.com>
2023-02-14 16:25:59 -0800

    created loading spinner icon

---
+++ src/components/inner/LoadingIcon.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../../loadingIcon.css"
+
+const LoadingIcon: React.FC = () => {
+    return (
+        <div id='loadingIcon'>
+
+        </div>
+    );
+}
+
+export default LoadingIcon;
