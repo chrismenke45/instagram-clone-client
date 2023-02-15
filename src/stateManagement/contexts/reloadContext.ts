3bfe112edd648240818e961db85e3d86fc1302b1
chrismenke45 <chrismenke45@gmail.com>
2023-02-14 17:46:53 -0800

    typo fix for variable name, no functional change

--- src/stateManagement/contexts/reloadContext.ts
+++ src/stateManagement/contexts/reloadContext.ts
@@ -1,14 +1,14 @@
 import { createContext } from "react";
 import { reloadStateInterface } from "../reducers/reloadReducer";
 
-export interface ImageCropperContextInterface {
+export interface ReloadContextInterface {
     reloadState: reloadStateInterface;
     reloadDispatch: React.Dispatch<any>;
   }
 const initialReloadState = {
     count: 0
 }
- const ReloadContext = createContext<ImageCropperContextInterface>({reloadState: initialReloadState, reloadDispatch: () => null})
+ const ReloadContext = createContext<ReloadContextInterface>({reloadState: initialReloadState, reloadDispatch: () => null})
 
 
 export default ReloadContext
