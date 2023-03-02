73332ff1c27a9eb0c54fb9058c9e7573e276521f
chrismenke45 <chrismenke45@gmail.com>
2023-03-02 14:24:52 -0800

    conversation page now loads more messages on scroll up

--- src/components/outer/Conversation.tsx
+++ src/components/outer/Conversation.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import getUserObject from '../../functions/user/getUserObject';
 import { ConversationProp } from '../../models/ConversationProp';
+import ReloadContext from '../../stateManagement/contexts/ReloadContext';
+import reloadActions from '../../stateManagement/actions/reloadActions';
 
 interface Props {
     messages: ConversationProp[];
+    setDisplayCount: React.Dispatch<React.SetStateAction<number>>;
 }
 const Conversation: React.FC<Props> = (props) => {
-    const { messages } = props
+    const { messages, setDisplayCount } = props
     const user = getUserObject()
+    const { reloadDispatch } = useContext(ReloadContext)
+
+    const scrollIncreaseDisplayCount = (e: React.UIEvent<HTMLElement>) => {
+        if (e.currentTarget.scrollHeight === e.currentTarget.clientHeight - e.currentTarget.scrollTop) {
+            setDisplayCount(prev => prev + 15);
+            reloadDispatch(reloadActions.INCREMENT())
+        }
+    }
 
     return (
-        <main id="conversation">
+        <main id="conversation" onScroll={scrollIncreaseDisplayCount}>
             <div className='pushToTop'></div> {/* This pushes messages to topof page if they don't fill the page*/}
             <ol>
                 {messages.map(message => {
--- src/pages/ConversationPage.tsx
+++ src/pages/ConversationPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useReducer } from 'react';
+import React, { useEffect, useState, useReducer, useContext } from 'react';
 import BackBanner from '../components/outer/BackBanner';
 import FetchAPI from '../functions/fetch/FetchAPI';
 import getUserObject from '../functions/user/getUserObject';
@@ -7,12 +7,16 @@ import { ProfileProp } from '../models/ProfileProp';
 import Conversation from '../components/outer/Conversation';
 import conversationReducer from '../stateManagement/reducers/conversationReducer';
 import conversationActions from '../stateManagement/actions/conversationActions';
+import generateQueryParams from '../functions/generateQueryParams';
+import ReloadContext from '../stateManagement/contexts/ReloadContext';
 
 
 const ConversationPage: React.FC = () => {
     const fetcher = new FetchAPI()
     const user = getUserObject()
     const [newMessage, setNewMessage] = useState<string>("")
+    const [displayCount, setDisplayCount] = useState<number>(25)
+    const { reloadState } = useContext(ReloadContext)
     const [otherUser, setOtherUser] = useState<ProfileProp>({
         username: "",
         name: "",
@@ -41,13 +45,16 @@ const ConversationPage: React.FC = () => {
                 .then(theOtherUser => {
                     setOtherUser(theOtherUser[0])
                 })
-            fetcher.fetchData(`users/${user.user_id}/messages/${user_id}`, "GET", user.jwt)
-                .then(theMessages => {
-                    conversationDispatch(conversationActions.SET_CONVERSATION(theMessages))
-                })
         }
 
     }, [])
+
+    useEffect(() => {
+        fetcher.fetchData(`users/${user.user_id}/messages/${user_id}${generateQueryParams({"count": displayCount})}`, "GET", user.jwt)
+                .then(theMessages => {
+                    conversationDispatch(conversationActions.SET_CONVERSATION(theMessages))
+                })
+    }, [reloadState.count])
 
 
     const handleNewMessageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -86,7 +93,7 @@ const ConversationPage: React.FC = () => {
     return (
         <div id="page">
             <BackBanner header={otherUser.username || 'Conversation'} img={otherUser.profile_picture} subHeader={otherUser.name} headerLink={`/profile/${otherUser.id}`} />
-            <Conversation messages={conversationState.conversation} />
+            <Conversation messages={conversationState.conversation} setDisplayCount={setDisplayCount}/>
             <form id="messageForm" onSubmit={handleSubmit}>
                 <input type="text" value={newMessage} onChange={handleNewMessageChange}></input>
                 <button type='submit'>Send</button>
