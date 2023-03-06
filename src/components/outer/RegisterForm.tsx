2f3f0f21fc9681b0cd2bfad949284a57f6813f94
chrismenke45 <chrismenke45@gmail.com>
2023-03-06 15:02:34 -0800

    added loading ui to sign up page

--- src/components/outer/RegisterForm.tsx
+++ src/components/outer/RegisterForm.tsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom"
 import FetchAPI from '../../functions/fetch/FetchAPI';
 import setUserJwt from '../../functions/user/setUserJwt';
 import { useNavigate } from 'react-router-dom';
+import LoadingIcon from '../inner/LoadingIcon';
 
 const RegisterForm: React.FC = () => {
     const [registerInfo, setRegisterInfo] = useState({ username: "", name: "", password: "" })
@@ -30,6 +31,8 @@ const RegisterForm: React.FC = () => {
     }
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        if (errorMessage) { return }
+        setLoading(true)
         e.preventDefault()
         fetcher.buildFormData([
             ["user[username]", registerInfo.username.toLowerCase().trim()],
@@ -54,6 +57,7 @@ const RegisterForm: React.FC = () => {
             })
     }
     const handleGuestLogin = () => {
+        setLoading(true)
         fetcher.loginAsGuest()
             .then(res => {
                 if (res.t) {
@@ -71,49 +75,54 @@ const RegisterForm: React.FC = () => {
     }
     return (
         <main id="register">
-            <h1>Instagram</h1>
-            <form onSubmit={handleSubmit}>
-                <div className='formGroup'>
-                    <input
-                        type="text"
-                        name="username"
-                        placeholder="Username"
-                        minLength={3}
-                        maxLength={30}
-                        value={registerInfo.username}
-                        onChange={handleChange}>
-                    </input>
-                </div>
-                <div className='formGroup'>
-                    <input
-                        type="text"
-                        name="name"
-                        placeholder="Name"
-                        minLength={3}
-                        maxLength={30}
-                        value={registerInfo.name}
-                        onChange={handleChange}>
-                    </input>
-                </div>
-                <div className='formGroup'>
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        minLength={6}
-                        maxLength={18}
-                        value={registerInfo.password}
-                        onChange={handleChange}>
-                    </input>
-                </div>
-                {errorMessage && <p className='error'>{errorMessage}</p>}
-                <button className="openerOption" type='submit'>Register</button>
-            </form>
-            <span>Already have an account?</span>
-            <Link to="/login" className='openerOption'>Login</Link>
-            <span>or</span>
-            <button onClick={handleGuestLogin} className='openerOption'>Login as Guest</button>
-
+            {loading ?
+                <LoadingIcon />
+                :
+                <>
+                    <h1>Instagram</h1>
+                    <form onSubmit={handleSubmit}>
+                        <div className='formGroup'>
+                            <input
+                                type="text"
+                                name="username"
+                                placeholder="Username"
+                                minLength={3}
+                                maxLength={30}
+                                value={registerInfo.username}
+                                onChange={handleChange}>
+                            </input>
+                        </div>
+                        <div className='formGroup'>
+                            <input
+                                type="text"
+                                name="name"
+                                placeholder="Name"
+                                minLength={3}
+                                maxLength={30}
+                                value={registerInfo.name}
+                                onChange={handleChange}>
+                            </input>
+                        </div>
+                        <div className='formGroup'>
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                                minLength={6}
+                                maxLength={18}
+                                value={registerInfo.password}
+                                onChange={handleChange}>
+                            </input>
+                        </div>
+                        {errorMessage && <p className='error'>{errorMessage}</p>}
+                        <button className="openerOption" type='submit'>Register</button>
+                    </form>
+                    <span>Already have an account?</span>
+                    <Link to="/login" className='openerOption'>Login</Link>
+                    <span>or</span>
+                    <button onClick={handleGuestLogin} className='openerOption'>Login as Guest</button>
+                </>
+            }
         </main>
     );
 }
