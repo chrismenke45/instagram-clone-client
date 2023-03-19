3d19308bbb35bf1088e2d58dbeded6381e588f73
chrismenke45 <chrismenke45@gmail.com>
2023-03-19 10:17:45 -0700

    function now gets user object each time

--- src/functions/eventHandlers/followHandlers.ts
+++ src/functions/eventHandlers/followHandlers.ts
@@ -1,12 +1,13 @@
 import getUserObject from "../user/getUserObject"
 import FetchAPI from "../fetch/FetchAPI"
 
-const user = getUserObject()
 const fetcher = new FetchAPI()
 
 export const follow = (profile_id: number) => {
+    const user = getUserObject()
     return fetcher.fetchData(`users/${profile_id}/follows`, "POST", user.jwt)
 }
 export const unfollow = (profile_id: number) => {
+    const user = getUserObject()
     return fetcher.fetchData(`users/${profile_id}/follows`, "DELETE", user.jwt)
 }
